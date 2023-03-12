0053407c64cb54b01fac3488348fefd190d97baf
amritpandey <amrit.pandey111@gmail.com>
2023-03-12 20:59:36 +0100

    Week2 feedback implemented

--- week2/exercise/Program.cs
+++ week2/exercise/Program.cs
@@ -21,31 +21,28 @@ app.MapGet("/calculator", (string numberOne, string numberTwo, string operation)
 
     if (!numberOneIsValid || !numberTwoIsValid)
     {
-       return Results.BadRequest("One of the numbers is not valid");
-       // Console.WriteLine("not valid numbers");
+        return Results.BadRequest("One of the numbers is not valid");
     }
 
     int result = 0;
 
     switch (operation)
     {
-        
+
         case "add":
             result = parseNumberOne + parseNumberTwo;
-    break;
+            break;
         case "sub":
             result = parseNumberOne - parseNumberTwo;
-    break;
-    case "mul":
+            break;
+        case "mul":
             result = parseNumberOne * parseNumberTwo;
-    break;
-         default:
-             return Results.BadRequest("Not valid operator!!!");
-             // Console.WriteLine("not valid operators");
-             
+            break;
+        default:
+            return Results.BadRequest("Not valid operator!!!");
+
     }
-    
-    //Console.WriteLine(result);
+
     return Results.Ok(result);
 });
 
@@ -61,44 +58,42 @@ Hint: use int.TryParse() and char.IsUpper()
 
 Example 1: GET /?input=153 would calculate 1 + 5 + 3 and return 9. Example 2: GET /?input=The Quick Brown Fox Jumps Over the Lazy Dog will return 8.*/
 
-app.MapGet("/methods", (string input) => {
+app.MapGet("/methods", (string input) =>
+{
     var inputIsNumber = int.TryParse(input, out var parseInput);
-    if(inputIsNumber){
-       return Results.Ok(AddNumbers(parseInput));
-    }else{
-       return Results.Ok(CountCapitalLetters(input));
+    if (inputIsNumber)
+    {
+        return Results.Ok(AddNumbers(parseInput));
+    }
+    else
+    {
+        return Results.Ok(CountCapitalLetters(input));
     }
 
 });
 
 
-
 int AddNumbers(int n)
-{
-   
-//var digits = n.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
 {
     int sum = 0;
     while (n > 0)
     {
-      sum = sum + n % 10;//153%10=3,5
-      n = n / 10;//15
+        sum = sum + n % 10;//153%10=3,5
+        n = n / 10;//15
     }
     return sum;
-  }
-
 }
 
 int CountCapitalLetters(string input)
 {
-   {
-    int count = 0;
-    foreach (var c in input)
     {
-      if (char.IsUpper(c)) count++;
+        int count = 0;
+        foreach (var c in input)
+        {
+            if (char.IsUpper(c)) count++;
+        }
+        return count;
     }
-    return count;
-  }
 
 }
 
@@ -108,24 +103,23 @@ Make a GET endpoint that takes a string as input and returns a new list containi
 sorted in alphabetical order. For example, if the input string is The cool breeze 
 whispered through the trees, the output should be ["b", "c", "d", "e", "h", "i", "l", "o", "p", "r", "s", "t", "u", "w", "z"].
 */
-app.MapGet("/dist-alphabet", (string input)=>{
+app.MapGet("/dist-alphabet", () =>
+{
+    string inputText = "The cool breeze whispered through the trees";
     var result = new List<char>();
     List<char> newList = new List<char>();
+    var input = inputText.ToLower().ToCharArray();
 
     foreach (var c in input)
     {
-        if(char.IsLetter(c))
+        if (char.IsLetter(c))
         {
-
             result.Add(c);
         }
-        
-       newList = result.Distinct().ToList();
-                newList.Sort();
-            
-       
     }
-     return Results.Ok(newList);
+    newList = result.Distinct().ToList();
+    newList.Sort();
+    return Results.Ok(newList);
 });
 app.Run();
 
