843e9f8f6ea61a6fb0aa424390ab6bf7fdeba242
amritpandey <amrit.pandey111@gmail.com>
2023-03-19 20:48:17 +0100

    Week4 homework

---
+++ week4/homework/Program.cs
@@ -0,0 +1,88 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello this is homework 4 World!");
+/*
+A POST endpoint that calls POST users/add with a record with FirstName, 
+LastName and Age (this simulates creating a user)
+*/
+app.MapPost("/create", async (User userData) =>
+{
+    var httpClient = new HttpClient();
+    var response= await httpClient.PostAsJsonAsync("https://dummyjson.com/users/add", userData)
+        ;
+        System.Console.WriteLine(response);
+        var data = await response.Content.ReadFromJsonAsync<User>();
+        var responseData = new
+        {
+            StatusCode = response.StatusCode,
+            ReasonPhrase = response.ReasonPhrase,
+            data
+
+        };
+        return (responseData);
+});
+/*
+A POST endpoint that that calls Post products/add with a record with Title and 
+Price (this simulates creating a product)
+*/
+app.MapPost("/createProduct", async (Product productData) =>
+{
+    var httpClient = new HttpClient();
+    var response= await httpClient.PostAsJsonAsync("https://dummyjson.com/products/add", productData)
+        ;
+        System.Console.WriteLine(response);
+        var data = await response.Content.ReadFromJsonAsync<Product>();
+        var responseData = new
+        {
+            StatusCode = response.StatusCode,
+            ReasonPhrase = response.ReasonPhrase,
+            data
+
+        };
+        return (responseData);
+});
+/*
+A POST endpoint that takes a lists of ids and retrieves 
+all of the users with those ids from the GET users (Id, FirstName, LastName and Age)*/
+app.MapPost("/retrieveUsers", async (UserId ids) =>
+{
+    var httpClient = new HttpClient();
+        var result = "";
+        foreach (var id in ids.Id)
+        
+        {
+            var response= await httpClient.GetAsync($"https://dummyjson.com/users/{id}");
+            var data = await response.Content.ReadFromJsonAsync<User>();
+             result += data.FirstName +" "+ data.LastName+" "+data.Age +" ";
+            
+        }
+        return Results.Ok(result); 
+});
+/*
+A POST endpoint that takes a lists of ids and retrieves all of the products 
+with those ids GET products(Id, Title)
+*/
+app.MapPost("/retrieveProducts", async (UserId ids) =>
+{
+    var httpClient = new HttpClient();
+        
+        List<string> productList = new List<string>();
+        foreach (var id in ids.Id)
+        
+        {
+            var response= await httpClient.GetAsync($"https://dummyjson.com/products/{id}");
+            var data = await response.Content.ReadFromJsonAsync<Product>();
+             var result = "Title: "+ data.Title +" Price: "+ data.Price+" ";
+             productList.Add(result);
+        }
+        return Results.Ok(productList); 
+});
+
+
+app.Run();
+
+record User(string FirstName, string LastName, int Age);
+record Product(string Title, decimal Price);
+record UserId(List<int>Id );
+record Product1(int Id, string Title, int Price);
