3d1d6c161b2e547fc547a999a546b51781aa763e
amritpandey <amrit.pandey111@gmail.com>
2023-04-09 20:24:21 +0200

    Added week6/namespace hw

--- week6/homework/Product.cs
+++ week6/homework/Product.cs
@@ -1,46 +1,55 @@
-
 using Dapper;
-using MySql.Data.MySqlClient;   
+using MySql.Data.MySqlClient;
 
 namespace HackYourFuture.Week6
 {
-public interface IProductRepository
-{
-    Task<IEnumerable<Product>> GetProducts();
-    Task<Product>PostUser(Product productPost);
-}
-
-public class ProductRepository : IProductRepository
-{
-    private string connectionString;
-
-    public ProductRepository(IConfiguration configuration)
+    public interface IProductRepository
     {
-        this.connectionString = configuration.GetConnectionString("Default");
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> PostProduct(Product productPost);
+        Task<Product> PutProduct(Product product);
+        Task<int> DeleteProduct(int id);
     }
 
-    public async Task<IEnumerable<Product>> GetProducts()
+    public class ProductRepository : IProductRepository
     {
-        using var connection = new MySqlConnection(connectionString);
+        private string connectionString;
 
-        var products = await connection.QueryAsync<Product>("SELECT id,name,price FROM products");
-        return products;
-    }
-
-    public async Task<Product>PostProduct(Product product)
-    {
-        using var connection = new MySqlConnection(connectionString);
-
-        var products = await connection.QueryAsync<Product>("INSERT INTO dapper.products (name, price) VALUES (@name, @price)", product);
-        return product;
-    }
-
-        public Task<Product> PostUser(Product productPost)
+        public ProductRepository(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            this.connectionString = configuration.GetConnectionString("Default");
         }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var products = await connection.QueryAsync<Product>("SELECT id,name,price FROM products");
+            return products;
+        }
+
+        public async Task<Product> PostProduct(Product product)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var products = await connection.ExecuteAsync("INSERT INTO products (name, price) VALUES (@name, @price)", product);
+            return product;
+        }
+
+        public async Task<Product> PutProduct(Product product)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var products = await connection.ExecuteAsync($"UPDATE products set name=@name, price=@price WHERE id=@id", product);
+            return product;
+        }
+
+        public async Task<int> DeleteProduct(int id)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var products = await connection.ExecuteAsync($"DELETE FROM products WHERE id=@id", new { id });
+            return products;
+        }
+
     }
-public record Product(int Id, string Name, decimal Price);
+    public record Product(int Id, string Name, decimal Price);
 }
 
 
--- week6/homework/Users.cs
+++ week6/homework/Users.cs
@@ -1,42 +1,55 @@
 
 using Dapper;
-using MySql.Data.MySqlClient;   
+using MySql.Data.MySqlClient;
 
 namespace HackYourFuture.Week6
 {
-public interface IUserRepository
-{
-    Task<IEnumerable<User>> GetUsers();
-    Task<User>PostUser(User userPost);
-}
-
-public class UserRepository : IUserRepository
-{
-    private string connectionString;
-
-    public UserRepository(IConfiguration configuration)
+    public interface IUserRepository
     {
-        this.connectionString = configuration.GetConnectionString("Default");
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> PostUser(User userPost);
+        Task<User> PutUser(User user);
+        Task<int> DeleteUser(int id);
     }
 
-    public async Task<IEnumerable<User>> GetUsers()
+    public class UserRepository : IUserRepository
     {
-        using var connection = new MySqlConnection(connectionString);
+        private string connectionString;
 
-        var users = await connection.QueryAsync<User>("SELECT first_name as FirstName,last_name as LastName,phone as Phone FROM dapper.users");
-        return users;
+        public UserRepository(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("Default");
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var users = await connection.QueryAsync<User>("SELECT id,name,phone FROM users1");
+            return users;
+        }
+
+        public async Task<User> PostUser(User user)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var users = await connection.ExecuteAsync("INSERT INTO dapper.users1 (name, phone) VALUES (@name, @phone)", user);
+            return user;
+        }
+
+        public async Task<User> PutUser(User user)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var users = await connection.ExecuteAsync($"UPDATE users1 set name=@name, phone=@phone WHERE id=@id", user);
+            return user;
+        }
+
+        public async Task<int> DeleteUser(int id)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            var users = await connection.ExecuteAsync($"DELETE FROM users1 WHERE id=@id", new { id });
+            return users;
+        }
     }
-
-    public async Task<User>PostUser(User user)
-    {
-        using var connection = new MySqlConnection(connectionString);
-
-        var users = await connection.QueryAsync<User>("INSERT INTO dapper.users (name, price) VALUES (@name, @price)", user);
-        return user;
-    }
-    
-}
-public record User(string FirstName, string LastName,int Phone);
+    public record User(int id, string name, int phone);
 }
 
 
